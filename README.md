98665e345b62784561783a5f124d6baa217d3491
flaireclair <h.kmntag6.gaburiasu@gmail.com>
2020-02-13 01:26:33 +0900

    シェルコマンドを用意

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-これを取り込んでおけばいいぞ!!!
+# ros2_base
 
+ros2を使うなら、とりあえずこれをクローンすればいいぞ！！！
+そしてこのコマンドをターミナルで実行してくれ！！！
+```
+bash ./setup.sh
+```
+
+##　サブモジュール
+[follow_me](https://github.com/rionehome/follow_me)
+[rione_msgs](https://github.com/rionehome/rione_msgs)
+[turtlebot_bringup](https://github.com/ItoMasaki/turtlebot_bringup)
---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+bringup_branch="bug_fix"
+
+cd follow_me
+branch=$(git branch --contains=HEAD)
+echo ${branch}
+if [ "${branch}" = "* ros2/master" ]; then
+    echo "pull!"
+    git pull origin ros2/master
+else
+    echo "Switch branches and then pull"
+    git checkout -b ros2/master
+    git pull origin ros2/master
+fi
+
+cd ../rione_msgs
+branch=$(git branch --contains=HEAD)
+echo ${branch}
+if [ "${branch}" = "* ros2/master" ]; then
+    echo "pull!"
+    git pull origin ros2/master
+else
+    echo "Switch branches and then pull"
+    git checkout -b ros2/master
+    git pull origin ros2/master
+fi
+
+cd ../turtlebot_bringup
+branch=$(git branch --contains=HEAD)
+echo ${branch}
+if [ "${branch}" = "* ${bringup_branch}" ]; then
+    echo "pull!"
+    git pull origin ${bringup_branch}
+else
+    echo "Switch branches and then pull"
+    git checkout -b ${bringup_branch}
+    git pull origin ${bringup_branch}
+fi
+
+cd ..
+
+colcon build
+
+source ./install/setup.bash
