07d5dc49f1783568fa13466bb200415aa8218215
flaireclair <h.kmntag6.gaburiasu@gmail.com>
2020-02-14 19:17:41 +0900

    サブモジュール追加

--- setup.sh
+++ setup.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
-
+followme_branch="ros2/master"
+msg_branch="ros2/master"
 bringup_branch="bug_fix"
+ydlider_branch="master"
+locationreg_branch="master"
+turn_branch="master"
 
 cd follow_me
 branch=$(git branch --contains=HEAD)
 echo ${branch}
-if [ "${branch}" = "* ros2/master" ]; then
+if [ "${branch}" = "* ${followme_branch}" ]; then
     echo "pull!"
-    git pull origin ros2/master
+    git pull origin ${followme_branch}
 else
     echo "Switch branches and then pull"
-    git checkout ros2/master
-    git pull origin ros2/master
+    git checkout ${followme_branch}
+    git pull origin ${followme_branch}
 fi
 
 cd ../rione_msgs
 branch=$(git branch --contains=HEAD)
 echo ${branch}
-if [ "${branch}" = "* ros2/master" ]; then
+if [ "${branch}" = "* ${msg_branch}" ]; then
     echo "pull!"
-    git pull origin ros2/master
+    git pull origin ${msg_branch}
 else
     echo "Switch branches and then pull"
-    git checkout ros2/master
-    git pull origin ros2/master
+    git checkout ${msg_branch}
+    git pull origin ${msg_branch}
 fi
 
 cd ../turtlebot_bringup
@@ -36,6 +40,42 @@ else
     echo "Switch branches and then pull"
     git checkout ${bringup_branch}
     git pull origin ${bringup_branch}
+fi
+
+cd ../ydlider_ros2
+branch=$(git branch --contains=HEAD)
+echo ${branch}
+if [ "${branch}" = "* ${ydlider_branch}" ]; then
+    echo "pull!"
+    git pull origin ${ydlider_branch}
+else
+    echo "Switch branches and then pull"
+    git checkout ${ydlider_branch}
+    git pull origin ${ydlider_branch}
+fi
+
+cd ../location_register
+branch=$(git branch --contains=HEAD)
+echo ${branch}
+if [ "${branch}" = "* ${locationreg_branch}" ]; then
+    echo "pull!"
+    git pull origin ${locationreg_branch}
+else
+    echo "Switch branches and then pull"
+    git checkout ${locationreg_branch}
+    git pull origin ${locationreg_branch}
+fi
+
+cd ../turn_robot
+branch=$(git branch --contains=HEAD)
+echo ${branch}
+if [ "${branch}" = "* ${turn_branch}" ]; then
+    echo "pull!"
+    git pull origin ${turn_branch}
+else
+    echo "Switch branches and then pull"
+    git checkout ${turn_branch}
+    git pull origin ${turn_branch}
 fi
 
 cd ..
